f4745b07d6d889ec21ec991d3fa965f9ba1a1163
Fábio Valentim <maitabom@gmail.com>
2025-11-15 21:42:55 -0300

    refatorar layout da página Pomodoro para usar Row em EntradaTempo

--- lib/components/entrada_tempo.dart
+++ lib/components/entrada_tempo.dart
@@ -8,6 +8,36 @@ class EntradaTempo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [Text(titulo), Text(valor.toString())]);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(titulo, style: TextStyle(fontSize: 25)),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(15),
+                backgroundColor: Colors.red,
+              ),
+              child: Icon(Icons.arrow_downward, color: Colors.white),
+            ),
+            SizedBox(height: 10),
+            Text('${valor.toString()} min', style: TextStyle(fontSize: 18)),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(15),
+                backgroundColor: Colors.red,
+              ),
+              child: Icon(Icons.arrow_upward, color: Colors.white),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
