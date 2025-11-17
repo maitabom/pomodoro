f711dcda223b3a6bdcb82e88bce2e9b360a489b6
Fábio Valentim <maitabom@gmail.com>
2025-11-16 22:09:57 -0300

    adicionar lógica de reinício e troca de intervalo no PomodoroStore e atualizar o componente Cronometro

--- lib/components/cronometro.dart
+++ lib/components/cronometro.dart
@@ -10,61 +10,64 @@ class Cronometro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final store = Provider.of<PomodoroStore>(context);
-    final titulo = store.estaTrabalhando() ? 'Trabalhando' : 'Descansando';
-    final colorStatus = store.estaTrabalhando() ? Colors.red : Colors.green;
 
-    return Container(
-      color: colorStatus,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(titulo, style: TextStyle(fontSize: 40, color: Colors.white)),
-          SizedBox(height: 20),
-          Text(
-            '${store.minutos.toString().padLeft(2, '0')}:${store.segundos.toString().padLeft(2, '0')}',
-            style: TextStyle(
-              fontSize: 120,
-              color: Colors.white,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          SizedBox(height: 20),
-          Observer(
-            builder: (_) => Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                if (!store.iniciado)
-                  Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: CronometroBotao(
-                      texto: 'Iniciar',
-                      icone: Icons.play_arrow,
-                      onClick: store.iniciar,
-                    ),
-                  ),
-                if (store.iniciado)
-                  Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: CronometroBotao(
-                      texto: 'Parar',
-                      icone: Icons.stop,
-                      onClick: store.parar,
-                    ),
-                  ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 10),
-                  child: CronometroBotao(
-                    texto: 'Reiniciar',
-                    icone: Icons.refresh,
-                    onClick: store.reiniciar,
-                  ),
+    return Observer(
+      builder: (_) {
+        final titulo = store.estaTrabalhando() ? 'Trabalhando' : 'Descansando';
+        final colorStatus = store.estaTrabalhando() ? Colors.red : Colors.green;
+
+        return Container(
+          color: colorStatus,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(titulo, style: TextStyle(fontSize: 40, color: Colors.white)),
+              SizedBox(height: 20),
+              Text(
+                '${store.minutos.toString().padLeft(2, '0')}:${store.segundos.toString().padLeft(2, '0')}',
+                style: TextStyle(
+                  fontSize: 120,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
                 ),
-              ],
-            ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (!store.iniciado)
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: CronometroBotao(
+                        texto: 'Iniciar',
+                        icone: Icons.play_arrow,
+                        onClick: store.iniciar,
+                      ),
+                    ),
+                  if (store.iniciado)
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: CronometroBotao(
+                        texto: 'Parar',
+                        icone: Icons.stop,
+                        onClick: store.parar,
+                      ),
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: CronometroBotao(
+                      texto: 'Reiniciar',
+                      icone: Icons.refresh,
+                      onClick: store.reiniciar,
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
--- lib/store/pomodoro.store.dart
+++ lib/store/pomodoro.store.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:mobx/mobx.dart';
 
 part 'pomodoro.store.g.dart';
@@ -21,41 +23,74 @@ abstract class _PomodoroStore with Store {
   bool iniciado = false;
 
   @observable
-  TipoIntervalo tipoIntervalo = TipoIntervalo.descanso;
+  TipoIntervalo tipoIntervalo = TipoIntervalo.trabalho;
+
+  Timer? cronometro;
 
   @action
   void incrementarTempoTrabalho() {
     tempoTrabalho++;
+
+    if (estaTrabalhando()) {
+      reiniciar();
+    }
   }
 
   @action
   void decrementarTempoTrabalho() {
     tempoTrabalho--;
+
+    if (estaTrabalhando()) {
+      reiniciar();
+    }
   }
 
   @action
   void incrementarTempoDescanso() {
     tempoDescanso++;
+
+    if (estaDescansando()) {
+      reiniciar();
+    }
   }
 
   @action
   void decrementarTempoDescanso() {
     tempoDescanso--;
+
+    if (estaDescansando()) {
+      reiniciar();
+    }
   }
 
   @action
   void iniciar() {
     iniciado = true;
+
+    cronometro = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (minutos == 0 && segundos == 0) {
+        _trocarTipoIntervalo();
+      } else if (minutos > 0 && segundos == 0) {
+        segundos = 59;
+        minutos--;
+      } else {
+        segundos--;
+      }
+    });
   }
 
   @action
   void parar() {
     iniciado = false;
+    cronometro?.cancel();
   }
 
   @action
   void reiniciar() {
-    iniciado = false;
+    parar();
+
+    minutos = estaTrabalhando() ? tempoTrabalho : tempoDescanso;
+    segundos = 0;
   }
 
   bool estaTrabalhando() {
@@ -64,6 +99,18 @@ abstract class _PomodoroStore with Store {
 
   bool estaDescansando() {
     return tipoIntervalo == TipoIntervalo.descanso;
+  }
+
+  void _trocarTipoIntervalo() {
+    if (estaTrabalhando()) {
+      tipoIntervalo = TipoIntervalo.descanso;
+      minutos = tempoDescanso;
+    } else {
+      tipoIntervalo = TipoIntervalo.trabalho;
+      minutos = tempoTrabalho;
+    }
+
+    segundos = 0;
   }
 }
 
